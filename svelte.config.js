4dd45df417c09f9a7ba871e896cba64ced268269
nemariia <mariianebesnaia@gmail.com>
2025-06-06 18:21:25 +0300

    Updated dependencies

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,15 @@ import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 const config = {
   preprocess: vitePreprocess(),
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		}),
   },
 };
 
